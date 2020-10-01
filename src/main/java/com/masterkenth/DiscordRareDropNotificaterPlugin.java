b9837f000c0358c32c378d7b376fb72aaf408079
Master Kenth <masterkenth@gmail.com>
2020-10-01 14:43:55 +0200

    chore: processLootDrop base

--- src/main/java/com/masterkenth/DiscordRareDropNotificaterPlugin.java
+++ src/main/java/com/masterkenth/DiscordRareDropNotificaterPlugin.java
@@ -1,6 +1,13 @@
 package com.masterkenth;
 
 import com.google.inject.Provides;
+import com.masterkenth.discord.Embed;
+import com.masterkenth.discord.Author;
+import com.masterkenth.discord.Field;
+import com.masterkenth.discord.Image;
+import com.masterkenth.discord.Webhook;
+
+import org.json.JSONObject;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
@@ -28,6 +35,8 @@ public class DiscordRareDropNotificaterPlugin extends Plugin {
 	@Override
 	protected void startUp() throws Exception {
 		log.info("Started");
+
+		ProcessLootDrop();
 	}
 
 	@Override
@@ -35,15 +44,69 @@ public class DiscordRareDropNotificaterPlugin extends Plugin {
 		log.info("Stopped");
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged) {
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "My wazzap plugin says " + config.greeting(), null);
-		}
-	}
-
 	@Provides
 	DiscordRareDropNotificaterConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(DiscordRareDropNotificaterConfig.class);
+	}
+
+	private void ProcessLootDrop() {
+		Author author = new Author();
+		author.setName("MK");
+		author.setIcon_url(getPlayerIcon());
+
+		Image thumbnail = new Image();
+		thumbnail.setUrl(getItemIcon());
+
+		Field rarityField = new Field();
+		rarityField.setName("Rarity");
+		rarityField.setValue(getItemRarityString());
+		rarityField.setInline(true);
+
+		Field valueField = new Field();
+		valueField.setName("Value");
+		valueField.setValue(getItemValueString());
+		valueField.setInline(true);
+
+		Embed embed = new Embed();
+		embed.setAuthor(author);
+		embed.setDescription(buildNotificationDescription());
+		embed.setThumbnail(thumbnail);
+		embed.setFields(new Field[]{ rarityField, valueField });
+		
+		Webhook webhookData = new Webhook();
+		webhookData.setEmbeds(new Embed[]{ embed });
+
+		ExecuteWebhook(webhookData);
+	}
+
+	private String buildNotificationDescription() {
+		// TODO: Return correct string
+		return "";
+	}
+
+	private String getItemValueString() {
+		// TODO: Return correct value string
+		return "";
+	}
+
+	private String getItemRarityString() {
+		// TODO: Return correct rarity string
+		return "";
+	}
+
+	private String getItemIcon() {
+		// TODO: Return correct item icon
+		return "";
+	}
+
+	private String getPlayerIcon() {
+		// TODO: Return approperiate ironman badge
+		return "";
+	}
+
+	private void ExecuteWebhook(Webhook webhook) {
+		JSONObject json = new JSONObject(webhook);
+		log.info("------ TEST JSON ------");
+		log.info(json.toString());
 	}
 }
