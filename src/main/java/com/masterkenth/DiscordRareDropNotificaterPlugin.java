35c4234c8102623ba9813065e2a70db6f2f64855
Master Kenth <masterkenth@gmail.com>
2020-10-01 17:00:50 +0200

    feat: basic loot check (wip)

--- src/main/java/com/masterkenth/DiscordRareDropNotificaterPlugin.java
+++ src/main/java/com/masterkenth/DiscordRareDropNotificaterPlugin.java
@@ -8,16 +8,25 @@ import com.masterkenth.discord.Image;
 import com.masterkenth.discord.Webhook;
 
 import org.json.JSONObject;
+
+import java.util.Collection;
+
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.ItemComposition;
+import net.runelite.api.NPC;
+import net.runelite.api.Player;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.NpcLootReceived;
+import net.runelite.client.events.PlayerLootReceived;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.loottracker.LootReceived;
+import net.runelite.http.api.loottracker.LootRecordType;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.ItemStack;
 
 @Slf4j
 @PluginDescriptor(
@@ -31,12 +40,15 @@ public class DiscordRareDropNotificaterPlugin extends Plugin {
 
 	@Inject
 	private DiscordRareDropNotificaterConfig config;
+	
+	@Inject
+	private ItemManager itemManager;
+
+	private final RarityChecker rarityChecker = new RarityChecker();
 
 	@Override
 	protected void startUp() throws Exception {
 		log.info("Started");
-
-		ProcessLootDrop();
 	}
 
 	@Override
@@ -47,6 +59,52 @@ public class DiscordRareDropNotificaterPlugin extends Plugin {
 	@Provides
 	DiscordRareDropNotificaterConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(DiscordRareDropNotificaterConfig.class);
+	}
+
+	@Subscribe
+	public void onNpcLootReceived(NpcLootReceived npcLootReceived)
+	{
+		NPC npc = npcLootReceived.getNpc();
+		Collection<ItemStack> items = npcLootReceived.getItems();
+
+		/* log.info("onNpcLootReceived: " + npc.getCombatLevel() + " " + npc.getName() + " dropped:"); */
+
+		for(ItemStack item : items) {
+			int id = item.getId();
+			ItemComposition itemComp = itemManager.getItemComposition(id);
+			/* log.info(item.getQuantity() + " " + itemComp.getName() + " (" + itemManager.getItemPrice(id) + ")"); */
+
+			log.info(npc.getName() + " (" + npc.getId() + ") dropped " + itemComp.getName() + " (" + item.getId() + ")");
+			QueueRarityCheck(npc.getId(), item.getId());
+		}
+	}
+
+	@Subscribe
+	public void onLootReceived(LootReceived lootReceived)
+	{
+		if(lootReceived.getType() != LootRecordType.EVENT) {
+			return;
+		}
+
+		// Only process EVENTS such as Barrows, CoX etc.
+		// For NPCs onNpcLootReceived receives more information and is used instead.
+
+		Collection<ItemStack> items = lootReceived.getItems();
+
+		log.info("OnLootReceived: " + lootReceived.getName() + " received:");
+
+		for(ItemStack item : items) {
+			int id = item.getId();
+			ItemComposition itemComp = itemManager.getItemComposition(id);
+			log.info(item.getQuantity() + " " + itemComp.getName() + " (" + itemManager.getItemPrice(id) + ")");
+		}
+	}
+
+	private void QueueRarityCheck(int npcId, int itemId) {
+		rarityChecker.CheckRarity(npcId, itemId)
+		.thenAccept(rarity -> {
+			log.info("npc " + npcId + " item " + itemId + " has rarity " + rarity + "(1/" + (1/rarity) + ")");
+		});
 	}
 
 	private void ProcessLootDrop() {
