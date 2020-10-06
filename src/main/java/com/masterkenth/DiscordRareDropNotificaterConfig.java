9d04193b737dca13d26c4bb5a08e7a004a50e863
Master Kenth <masterkenth@gmail.com>
2020-10-06 15:13:18 +0200

    chore: config setup

--- src/main/java/com/masterkenth/DiscordRareDropNotificaterConfig.java
+++ src/main/java/com/masterkenth/DiscordRareDropNotificaterConfig.java
@@ -6,8 +6,8 @@ import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("discordraredropnotificater")
 public interface DiscordRareDropNotificaterConfig extends Config {
-	@ConfigItem(keyName = "greeting", name = "Welcome Greeting", description = "The message to show to the user when they login")
-	default String greeting() {
-		return "Hello";
+	@ConfigItem(keyName = "webhookurl", name = "Discord webhook URL", description = "The Discord Webhook URL to use")
+	default String webhookUrl() {
+		return "";
 	}
 }
