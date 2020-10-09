2ef8d872f7e7b9a5e09b27c3cf5cb75ba1b21f48
Master Kenth <masterkenth@gmail.com>
2020-10-09 12:42:33 +0200

    fix: check rarity with variation ids
    notably fixes coin drops (id 617 vs 995)

--- src/main/java/com/masterkenth/RarityChecker.java
+++ src/main/java/com/masterkenth/RarityChecker.java
@@ -1,12 +1,15 @@
 package com.masterkenth;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.Collection;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import lombok.extern.slf4j.Slf4j;
+
+import net.runelite.client.game.ItemVariationMapping;
 
 @Slf4j
 public class RarityChecker
@@ -43,17 +46,22 @@ public class RarityChecker
       try
       {
         JSONArray drops = npcJson.getJSONArray("drops");
+        int mapId = ItemVariationMapping.map(itemId);
+        Collection<Integer> idVariations = ItemVariationMapping.getVariations(mapId);
 
         for (int i = 0; i < drops.length(); i++)
         {
           JSONObject drop = drops.getJSONObject(i);
           int dropId = drop.getInt("id");
-          if (dropId == itemId)
+          if (idVariations.contains(dropId))
           {
             float rarity = drop.getFloat("rarity");
             f.complete(rarity);
+            return;
           }
         }
+        // No entry for item, default to 100% drop
+        f.complete(1.0f);
       }
       catch (Exception e)
       {
